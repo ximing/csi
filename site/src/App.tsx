b4ad37713139834099c83e5e643836ec00c638b4
ximing <ximing@meituan.com>
2026-03-19 14:00:00 +0800

    site features：四张卖点卡

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import Features from './components/Features'
 
 export default function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Features />
     </>
   )
 }
