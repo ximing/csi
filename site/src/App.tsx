62346ee6a7092bde77b5a50149651b65b0f4134e
ximing <ximing@meituan.com>
2026-03-20 14:00:00 +0800

    site tools：17 工具网格

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -2,6 +2,7 @@ import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import Architecture from './components/Architecture'
+import Tools from './components/Tools'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Hero />
       <Features />
       <Architecture />
+      <Tools />
     </>
   )
 }
